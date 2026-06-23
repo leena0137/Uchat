9ebfc0c790168f680acccbecfebf18e88e7b71f8
Leena0137 <leena0137@github.com>
2026-06-23 15:45:43 +0530

    Fix crash

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { StatusBar } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import AppNavigator from './src/navigation/AppNavigator';
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+      <StatusBar style="light" />
       <AppNavigator />
     </SafeAreaProvider>
   );
